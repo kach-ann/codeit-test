43039b44cc01a151f7af13ecfd42dee1dd177329
kach-ann <sinkoa234@gmail.com>
2018-09-20 15:40:00 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import 'bootstrap';
 import './scss/index.scss';
 
 import { registration } from './registration/registration';
-import { companies } from './companies/companies';
 
 $(function () {
   registration.render();
-  companies.render();
 });
